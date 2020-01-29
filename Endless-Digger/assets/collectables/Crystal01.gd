6e66deec1b510d64e499a0fd973815a50f3f904a
AgentSasori <info@trinium.de>
2020-01-29 20:12:16 +0100

    crystal script fehler behoben

--- Endless-Digger/assets/collectables/Crystal01.gd
+++ Endless-Digger/assets/collectables/Crystal01.gd
@@ -2,9 +2,11 @@ extends RigidBody2D
 
 export var _value = 10
 
-var resource = {type: "crystal", value: _value}
+var resource = {"type": "crystal", "value": _value}
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("player"):
-		print("player colledted ", self.name)
+		print("player collected ", self.name)
+		print("Type: ", resource.type)
+		print("Value: ", resource.value)
 		queue_free()
